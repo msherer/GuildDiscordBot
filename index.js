e717352cee4375f50d701f478a819f0b78ef41e0
Matt Sherer <msherer033@gmail.com>
2020-05-11 17:32:38 -0500

    Breaking out Roster configs and updating the global dependencies to inclue the roster

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const Discord = require('discord.js');
 const auth = require('./config/auth.json');
-const config = require('./config/config.json');
+const generalConfig = require('./config/config.json');
+const craftConfig = require('./config/craft.json');
+const rosterConfig = require('./config/roster.json');
 const client = new Discord.Client();
 const commandContainer = new (require('./src/CommandContainer'));
 
 const dependencyMap = {
-	'Discord': Discord,
-	'discordClient': client,
-	'config': config,
-	'prefix': config.prefix,
-	'commandList': config.commandsList
+    'Discord': Discord,
+    'discordClient': client,
+    'config': generalConfig,
+    'craft': craftConfig,
+    'roster': rosterConfig,
+    'prefix': generalConfig.prefix,
+    'commandList': generalConfig.commandsList
 };
 
-for (const key in config.commandsList) {
-	const command = new (require(`./src/Commands/${config.commandsList[key]}`));
-	command.initialize(dependencyMap);
-	commandContainer.add(config.commandsList[key], command);
+for (const key in generalConfig.commandsList) {
+    const command = new (require(`./src/Commands/${generalConfig.commandsList[key]}`));
+    command.initialize(dependencyMap);
+    commandContainer.add(generalConfig.commandsList[key], command);
 }
 
 client.on('ready', () => {
--- src/Commands/Roster.js
+++ src/Commands/Roster.js
@@ -14,7 +14,7 @@ class Roster extends Command
 			"class": "class",
 			"spec": "spec"
 		};
-		const roster = this.dependencies.config.roster;
+		const roster = this.dependencies.roster;
 		roster.unshift(header);
 		
 		const colWidth = 20;

969f869fba2157528aa37f6756556800a5c7a27d
Matt Sherer <msherer033@gmail.com>
2020-05-16 19:22:04 -0500

    Adding logging and reconnection

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const craftConfig = require('./config/craft.json');
 const rosterConfig = require('./config/roster.json');
 const client = new Discord.Client();
 const commandContainer = new (require('./src/CommandContainer'));
+const botPrefix = '!gdb';
 
 const dependencyMap = {
     'Discord': Discord,
@@ -23,7 +24,25 @@ for (const key in generalConfig.commandsList) {
 }
 
 client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+	client.user.setActivity(
+            `${botPrefix} commands | Running on ${client.guilds.cache.size} servers`
+	);
+    console.log(client.guilds.cache.size);
     console.log('Guild Discord Bot Bot Loaded!');
 });
+
+client
+    .on("error", console.error)
+    .on("warn", console.warn)
+    .on("debug", console.log);
+
+client
+    .on("reconnecting", () => {
+        console.warn("GuildDiscordBot is reconnecting...");
+    })
+    .on("disconnect", () => {
+        console.warn("Warning! GuildDiscordBot has disconnected!");
+    });
 
 client.login(auth.token);
